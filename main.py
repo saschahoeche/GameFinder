c32479fcb33a9a26429220dbd91368b5b7ef5fa9
Sascha Hoeche <sascha.hoeche@gmx.com>
2020-08-13 11:12:28 +0200

    Switched to PySimpleGUI

--- main.py
+++ main.py
@@ -1,77 +1,60 @@
-import wx
+import PySimpleGUI as sg
 import requests
+import time
 
+# base variables
+apiServerTestURL = "https://api.rawg.io/api/games/grand-theft-auto-v"
+testResponse = requests.get(apiServerTestURL)
 
-class PanelOne(wx.Panel):
-    def __init__(self, parent):
-        wx.Panel.__init__(self, parent=parent)
-        inputText = wx.TextCtrl(self)
-        enterButton = wx.Button(self, label="Enter")
+col = [
+    [sg.Text("GameFinder", justification="center", font=("", 36))],
+    [sg.Button("Check Connectivity", font=("", 16), auto_size_button=True, key="_CONN_CHECK_")],
+    [sg.Output(size=(80, 20), font=("", 14))]
+    ]
 
+layout = [
+    [sg.Column(col, element_justification="center")]
+]
 
-class PanelTwo(wx.Panel):
-    def __init__(self, parent):
-        wx.Panel.__init__(self, parent=parent)
+'''
+def jsonCaller():
+    apiServerURL = "https://api.rawg.io/api/games/grand-theft-auto-v"
 
-        nameOfGame = wx.StaticText(self, label="Name: ")
-        nameOutput = wx.StaticText(self, label=GAMENAME)
+    response = requests.get(apiServerURL)
 
-        metaScore = wx.StaticText(self, label="Score: ")
-        scoreOutput = wx.StaticText(self, label=SCORE)
+    # check if server response, then get json
+    if response.status_code != 200:
+        print("[-] Can't connect to API Server, try again later.")
+    else:
+        print("[+] Successfully Connected to Server \n")
+        dataJson = response.json()
+        gameName = dataJson["name_original"]
+        # description = dataJson["description_raw"]
+        metacritic = dataJson["metacritic"]
+        releaseDate = dataJson["released"]
+        gameImg = dataJson["background_image"]
 
-        releaseDate = wx.StaticText(self, label="Released: ")
-        dateOutput = wx.StaticText(self, label=DATE)
+    print(gameName, metacritic, releaseDate, gameImg)
+'''
 
-        descr = wx.StaticText(self, label="Description: ")
-        descrOutput = wx.StaticText(self, label=DESCR)
+# MainFrame
+window = sg.Window("Game - Finder", layout)
+checkEnable = True
 
+def checkConnection():
+    if testResponse.status_code != 200:
+        print("[-] Can't connect to API Server, try again later.")
+    else:
+        print("[+] Successfully Connected to Server \n")
 
-class MainFrame(wx.Frame):
-    def __init__(self):
-        super().__init__(parent=None, title='Game Finder')
-        panel = wx.Panel(self)
-        mainFrameSizer = wx.BoxSizer(wx.VERTICAL)
+# MainLoop
+while True:
+    event, values = window.read() # write events/values in respective variables
+    # end program wenn window is closed or button pressed
+    if event == sg.WIN_CLOSED or event == "_EXIT_":
+        break
+    if event == "_CONN_CHECK_":
+        checkConnection()
+        window.Refresh()
 
-        self.textInput = wx.TextCtrl(panel, pos=(5, 5))
-        mainFrameSizer.Add(self.textInput, 0, wx.ALL | wx.EXPAND, 5)
-
-        enterBtn = wx.Button(panel, label="Enter", pos=(5, 55))
-        enterBtn.Bind(wx.EVT_BUTTON, self.enterPress)
-        mainFrameSizer.Add(enterBtn, 0, wx.ALL | wx.CENTER, 5)
-
-        panel.SetSizer(mainFrameSizer)
-        self.Show()
-
-    # define press action on enter button
-    def enterPress(self, event):
-        value = self.textInput.GetValue()
-        if not value:
-            print("Please enter the name of a Game first.")
-        else:
-            print(f"You are looking for {value}.")
-
-    # ToDo integrate caller script
-    def jsonCaller():
-        apiServerURL = "https://api.rawg.io/api/games/grand-theft-auto-v"
-
-        response = requests.get(apiServerURL)
-
-        # check if server response, then get json
-        if response.status_code != 200:
-            print("[-] Can't connect to API Server, try again later.")
-        else:
-            print("[+] Successfully Connected to Server \n")
-            dataJson = response.json()
-            gameName = dataJson["name_original"]
-            # description = dataJson["description_raw"]
-            metacritic = dataJson["metacritic"]
-            releaseDate = dataJson["released"]
-            gameImg = dataJson["background_image"]
-
-        print(gameName, metacritic, releaseDate, gameImg)
-
-
-if __name__ == '__main__':
-    app = wx.App()
-    frame = MainFrame()
-    app.MainLoop()
+window.close()
